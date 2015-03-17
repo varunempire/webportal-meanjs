560fc451db9fcac24d281826d2e4d269c246a97c
Vinoth Kumar <vinothapplife@gmail.com>
2015-03-17 21:27:50 +0530

    addbook implemented

--- public/modules/addbooks/config/addbooks.client.config.js
+++ public/modules/addbooks/config/addbooks.client.config.js
@@ -7,8 +7,8 @@ angular.module('addbooks').run(['Menus',
 		Menus.addMenuItem('topbar', 'Library', 'addbooks', 'dropdown', '/addbooks(/create)?');
 		Menus.addSubMenuItem('topbar', 'addbooks', 'Search Books', 'addbooks');
 		Menus.addSubMenuItem('topbar', 'addbooks', 'Add Books', 'addbooks/create');
-		Menus.addSubMenuItem('topbar', 'addbooks', 'Issue Book', 'addbooks/issue');
+		/*Menus.addSubMenuItem('topbar', 'addbooks', 'Issue Book', 'addbooks/issue');
 		Menus.addSubMenuItem('topbar', 'addbooks', 'Return Book', 'addbooks/return');
-		Menus.addSubMenuItem('topbar', 'addbooks', 'Book Renewal book', 'addbooks/renewal');
+		Menus.addSubMenuItem('topbar', 'addbooks', 'Book Renewal book', 'addbooks/renewal');*/
 	}
 ]);
