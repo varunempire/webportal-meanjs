29a9c1482aa2e563be4556a57097584cde8d0c33
yoganand-intellify <yoganand.p@htcindia.com>
2015-03-27 09:42:55 +0530

    lib files for file upload

--- config/env/all.js
+++ config/env/all.js
@@ -17,9 +17,9 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
 			],
 			js: [
-				'public/lib/ng-file-upload/angular-file-upload-shim.min.js',
+				'public/vendor/ng-file-upload/angular-file-upload-shim.min.js',
 				'public/lib/angular/angular.js',
-				'public/lib/ng-file-upload/angular-file-upload.min.js',
+				'public/vendor/ng-file-upload/angular-file-upload.min.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-cookies/angular-cookies.js',
 				'public/lib/angular-animate/angular-animate.js',
@@ -28,7 +28,7 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/ng-flow/dist/ng-flow-standalone.js',
+				'public/vendor/ng-flow/dist/ng-flow-standalone.js',
 				
 			]
 		},
